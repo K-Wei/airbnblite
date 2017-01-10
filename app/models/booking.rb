afef874659cda11e72fc89ec1aff6e183963d89f
Raghu Betina <raghu@firstdraft.com>
2017-01-10 05:17:26 +0000

    Direct association between bookings and guestreviews

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,9 @@
 class Booking < ApplicationRecord
   # Direct associations
 
+  has_one    :guestreview,
+             :dependent => :destroy
+
   has_one    :listingreview,
              :dependent => :destroy
 
