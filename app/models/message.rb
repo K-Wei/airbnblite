3d5b15a57f24f155e692b55520ee1f4c7dc35bdb
Raghu Betina <raghu@firstdraft.com>
2017-01-10 05:17:26 +0000

    Direct association between msgrecievers and messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ApplicationRecord
   # Direct associations
 
+  belongs_to :reciever,
+             :class_name => "Msgreciever"
+
   belongs_to :sender,
              :class_name => "Msgsender"
 
--- app/models/msgreciever.rb
+++ app/models/msgreciever.rb
@@ -1,6 +1,10 @@
 class Msgreciever < ApplicationRecord
   # Direct associations
 
+  has_many   :messages,
+             :foreign_key => "reciever_id",
+             :dependent => :destroy
+
   belongs_to :guest
 
   belongs_to :host
