0e55eb93e7de0349f4a11e8b3291066c7172414a
Raghu Betina <raghu@firstdraft.com>
2017-01-10 05:18:14 +0000

    Whitelist columns in Guestreview for ActiveAdmin

--- app/admin/guestreview.rb
+++ app/admin/guestreview.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Guestreview do
 
+ permit_params :host_id, :guest_id, :overall_rating, :booking_id, :review_content, :photos
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
