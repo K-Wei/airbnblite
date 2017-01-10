d5e8e71d9ae0ba331394457c5459bea06fb6d97a
Raghu Betina <raghu@firstdraft.com>
2017-01-10 05:17:26 +0000

    Direct association between msgsenders and messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ApplicationRecord
   # Direct associations
 
+  belongs_to :sender,
+             :class_name => "Msgsender"
+
   # Indirect associations
 
   # Validations
--- app/models/msgsender.rb
+++ app/models/msgsender.rb
@@ -1,6 +1,10 @@
 class Msgsender < ApplicationRecord
   # Direct associations
 
+  has_many   :messages,
+             :foreign_key => "sender_id",
+             :dependent => :destroy
+
   belongs_to :guest
 
   belongs_to :host
